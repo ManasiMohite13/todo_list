130b3a39a37ee2e43943e8bb76929bfe7827d73d
ManasiMohite13 <manasimohite3553@gmail.com>
2025-03-18 17:55:11 +0530

    Added Local storage support

--- script.js
+++ script.js
@@ -10,20 +10,31 @@ function addTask() {
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
+        span.innerHTML = "\u00d7";  // Unicode for '×' (delete icon)
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData(); // Save tasks after adding
 }
 
 listContainer.addEventListener("click", function (e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData(); // Save tasks after checking/unchecking
     }
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
+        saveData();// Save tasks after deleting
     }
 }, false);
 
-
-
+// Function to save tasks in localStorage
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+// Function to load tasks from localStorage
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+//Loads the task when the page loads
+showTask();
